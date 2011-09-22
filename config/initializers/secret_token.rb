9a3df44cb00339a9cd53a318d04f24fa584bfe49
Nikki Forshaw <nikki@electricpencil.co.za>
2011-09-22 11:32:38 +0200

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LittleStar::Application.config.secret_token = '4c34e8784c974b31e5776c065dcf0237794496e65dd52a41079ac32008a021142df06113f083b3b608b8de270cb0a9b081bfad8a71a65681b5b18c46d63f2aee'
